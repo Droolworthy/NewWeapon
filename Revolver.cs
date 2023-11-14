e3f788d7d47bcdd5baf1ae8c2c9592355c9b2e90
Droolworthy <107348019+Droolworthy@users.noreply.github.com>
2023-11-14 23:44:04 +0300

    Update Revolver.cs

--- Revolver.cs
+++ Revolver.cs
@@ -1,35 +1,9 @@
 using UnityEngine;
 
-[RequireComponent (typeof(Animator))]
-public class AttackState : State
+public class Revolver : Weapon
 {
-    [SerializeField] private int _damage;
-    [SerializeField] private float _delay;
-
-    private float _lastAssaultTime;
-    private Animator _animator;
-
-    private void Start()
+    public override void Shoot(Transform shootPoint)
     {
-        _animator = GetComponent<Animator>();
-    }
-
-    private void Update()
-    {
-        if(_lastAssaultTime <= 0)
-        {
-            Assault(Target);
-
-            _lastAssaultTime = _delay;
-        }
-
-        _lastAssaultTime -= Time.deltaTime;  
-    }
-
-    private void Assault(Player target)
-    {
-        _animator.Play("Assault");
-
-        target.ApplyDamage(_damage);
+        Instantiate(RevolverBullet, shootPoint.position, Quaternion.identity);
     }
 }
